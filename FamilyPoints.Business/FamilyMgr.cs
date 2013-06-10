19595acde9f4932372ae246e7f984150f4295699
fb52175 <fb52175@outlook.com>
2013-06-09 22:36:11 -0500

    Business Manager cleanup

---
+++ FamilyPoints.Business/FamilyMgr.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FamilyPoints.Domain;
+using FamilyPoints.Service;
+
+namespace FamilyPoints.Business
+{
+    public class FamilyMgr
+    {
+        public void Create(Family family)
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+
+            familySvc.Insert(family);
+            familySvc.Save();
+        }
+
+        public void Update(Family family, string name)
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+            family.Name = name;
+            familySvc.Update(family);
+            familySvc.Save();
+        }
+
+        public void AddChild(Family family, Child child)
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+            family.Children.Add(child);
+            familySvc.Update(family);
+            familySvc.Save();
+        }
+
+        public void AddParent(Family family, Parent parent)
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+            family.Parents.Add(parent);
+            familySvc.Update(family);
+            familySvc.Save();
+        }
+
+        public void Delete(Family family)
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+
+            familySvc.Delete(family);
+            familySvc.Save();
+        }
+
+        public Family Find(int id)
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+            Family family = familySvc.GetById(id);
+            //familySvc.Dispose();
+            return family;
+        }
+
+        public IEnumerable<Family> GetFamilies()
+        {
+            Factory factory = Factory.GetInstance();
+            IFamilySvc familySvc = (IFamilySvc)factory.GetService("IFamilySvc");
+            return familySvc.GetFamilies();
+        }
+    }
+}
---
+++ FamilyPoints.Business/TransactionMgr.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FamilyPoints.Domain;
+using FamilyPoints.Service;
+
+namespace FamilyPoints.Business
+{
+    public class TransactionMgr
+    {
+        public void Create(Transaction transaction)
+        {
+            Factory factory = Factory.GetInstance();
+            ITransactionSvc transactionSvc = (ITransactionSvc)factory.GetService("ITransactionSvc");
+
+            transactionSvc.Insert(transaction);
+            transactionSvc.Save();
+        }
+
+        public void Update(Transaction transaction, string description, int points)
+        {
+            Factory factory = Factory.GetInstance();
+            ITransactionSvc transactionSvc = (ITransactionSvc)factory.GetService("ITransactionSvc");
+            transaction.Description = description;
+            transaction.Points = points;
+            transactionSvc.Update(transaction);
+            transactionSvc.Save();
+        }
+
+        public void Delete(Transaction transaction)
+        {
+            Factory factory = Factory.GetInstance();
+            ITransactionSvc transactionSvc = (ITransactionSvc)factory.GetService("ITransactionSvc");
+
+            transactionSvc.Delete(transaction);
+            transactionSvc.Save();
+        }
+
+        public Transaction Find(int id)
+        {
+            Factory factory = Factory.GetInstance();
+            ITransactionSvc transactionSvc = (ITransactionSvc)factory.GetService("ITransactionSvc");
+            return transactionSvc.GetById(id);
+        }
+
+        public IEnumerable<Transaction> GetTransactions()
+        {
+            Factory factory = Factory.GetInstance();
+            ITransactionSvc transactionSvc = (ITransactionSvc)factory.GetService("ITransactionSvc");
+            return transactionSvc.GetTransactions();
+        }
+    }
+}
